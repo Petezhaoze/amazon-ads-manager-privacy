3665200afc26ab37f947386bbc61bfa47eeab4c3
Zeyu <zzhao84@jh.edu>
2026-05-13 00:22:02 -0400

    Fix HttpClient base address resolving to file:// in WebAssembly

--- src/AmazonAdsManager.Client/Program.cs
+++ src/AmazonAdsManager.Client/Program.cs
@@ -7,8 +7,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var apiBase = builder.Configuration["ApiBaseUrl"]
-    ?? builder.HostEnvironment.BaseAddress.TrimEnd('/') + "/api";
+var configuredPath = builder.Configuration["ApiBaseUrl"] ?? "/api";
+var apiBase = configuredPath.StartsWith("http")
+    ? configuredPath
+    : new Uri(new Uri(builder.HostEnvironment.BaseAddress), configuredPath).ToString();
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(apiBase) });
 builder.Services.AddScoped<AdsApiClient>();
